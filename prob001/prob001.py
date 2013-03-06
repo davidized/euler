8590d5f5b454d404c30a3aa4615b1b490f11b6cb
David Williamson <david@davidized.com>
2013-03-06 15:11:36 -0600

    Problem 1

---
+++ prob001/prob001.py
@@ -0,0 +1,11 @@
+sum = 0
+for i in range(0,1000):
+    if (i % 3) == 0:
+        sum = sum + i
+#        print("i3: " . i . "\n")
+    elif (i % 5) == 0:
+        sum = sum + i
+#        print("i5: " . i . "\n")
+
+print(sum)    
+
